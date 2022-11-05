5cfd97d57fce293f83a7bce171633b62b5223fa1
emquynhbuidoi <lequynhle12345678@gmail.com>
2022-11-05 10:04:36 +0700

    ngay 5-11

---
+++ alo alo/Controllers/CartController.cs
@@ -0,0 +1,103 @@
+﻿using alo_alo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace alo_alo.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public List<CartItem> GetCart()
+        {
+            List<CartItem> myCart = Session["GioHang"] as
+            List<CartItem>;
+            //Nếu giỏ hàng chưa tồn tại thì tạo mới và đưa vào Session
+            if (myCart == null)
+            {
+                myCart = new List<CartItem>();
+                Session["GioHang"] = myCart;
+            }
+            return myCart;
+        }
+
+        public ActionResult AddToCart(int id)
+        {
+            //Lấy giỏ hàng hiện tại
+            List<CartItem> myCart = GetCart();
+            CartItem currentProduct = myCart.FirstOrDefault(p =>
+            p.ProductID == id);
+            if (currentProduct == null)
+            {
+                currentProduct = new CartItem(id);
+                myCart.Add(currentProduct);
+            }
+            else
+            {
+                currentProduct.Number++; //Sản phẩm đã có trong giỏ thì tăng số lượng lên 1
+            }
+            return RedirectToAction("Index", "CustomerProducts", new
+            {
+                id = id
+            });
+        }
+
+        private int GetTotalNumber()
+        {
+            int totalNumber = 0;
+            List<CartItem> myCart = GetCart();
+            if (myCart != null)
+                totalNumber = myCart.Sum(sp => sp.Number);
+            return totalNumber;
+        }
+
+        private decimal GetTotalPrice()
+        {
+            decimal totalPrice = 0;
+            List<CartItem> myCart = GetCart();
+            if (myCart != null)
+                totalPrice = myCart.Sum(sp => sp.FinalPrice());
+            return totalPrice;
+        }
+
+        //Xây dựng hàm hiển thị thông tin bên trong giỏ hàng
+        public ActionResult GetCartInfo()
+        {
+            List<CartItem> myCart = GetCart();
+            //Nếu giỏ hàng trống thì trả về trang ban đầu
+            if (myCart == null || myCart.Count == 0)
+            {
+                return RedirectToAction("Index", "CustomerProducts");
+            }
+            ViewBag.TotalNumber = GetTotalNumber();
+            ViewBag.TotalPrice = GetTotalPrice();
+            return View(myCart); //Trả về View hiển thị thông tin giỏhàng
+        }
+
+
+
+
+
+        //Hiển thị logo xe đẩy
+        public ActionResult CartPartial()
+        {
+            ViewBag.TotalNumber = GetTotalNumber();
+            ViewBag.TotalPrice = GetTotalPrice();
+            return PartialView();
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ alo alo/Controllers/CustomerProductsController.cs
@@ -0,0 +1,49 @@
+﻿using alo_alo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+
+namespace alo_alo.Controllers
+{
+    public class CustomerProductsController : Controller
+    {
+        private DBSportStoreEntities db = new DBSportStoreEntities();
+        // GET: CustomerProducts
+        public ActionResult Index()
+        {
+            var products = db.Products;
+            return View(products.ToList());
+        }
+
+        public ActionResult Details(int id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product pro = db.Products.Find(id);
+            if(pro == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pro);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
